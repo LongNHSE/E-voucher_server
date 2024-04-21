3b993ac609efab495fd456c8dcd880da00edaddf
PhiNSE <93396482+PhiNSE@users.noreply.github.com>
2024-04-21 17:44:08 +0700

    image[PhiN]

--- src/common/util/FirebaseUtil.ts
+++ src/common/util/FirebaseUtil.ts
@@ -1,7 +1,26 @@
-import { getDownloadURL, getStorage, ref, uploadBytes } from 'firebase/storage';
+import { initializeApp } from 'firebase/app';
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+} from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDOkC-FMpYNyDb_TYPiZ-m_EoifRTzVsjA',
+  authDomain: 'kohineko-7d678.firebaseapp.com',
+  projectId: 'kohineko-7d678',
+  storageBucket: 'kohineko-7d678.appspot.com',
+  messagingSenderId: '597681719456',
+  appId: '1:597681719456:web:3c099019e2888fb36461c5',
+  measurementId: 'G-X221KMS0F4',
+};
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app);
 
 export const uploadImage = async (file: File, directory: string = 'images') => {
-  const storage = getStorage();
   const fileName = Date.now();
   const storageRef = ref(storage, `${directory}/${fileName}`);
   await uploadBytes(storageRef, file);
